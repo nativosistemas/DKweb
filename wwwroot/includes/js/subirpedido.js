953bebb0da3aacbd58179af659474a99ddd69968
nativosistemas <nativosistemas@outlook.com.ar>
2023-05-16 00:47:17 -0300

    pass

--- wwwroot/includes/js/subirpedido.js
+++ wwwroot/includes/js/subirpedido.js
@@ -47,6 +47,24 @@ jQuery(document).ready(function () {
 
 });
 
+$(document).ready(function () {
+    $("#btn_MuestraExcel").click(function (e) {
+        e.preventDefault();
+
+        if ($("#divMuestraExcel").is(":visible")) {
+            $("#divMuestraExcel").hide();
+        } else {
+            $("#divMuestraExcel").show();
+            $("#divMuestraOtroSistemas").hide();
+        }
+    });
+
+    $('#btn_MuestraOtroSistemas').click(function () {
+        $("#divMuestraOtroSistemas").toggle();
+        $("#divMuestraExcel").hide();
+        return false;
+    });
+});
 $('#btn_MuestraOtroSistemas').click(function () {
     $("#divMuestraOtroSistemas").toggle();
     return false;
